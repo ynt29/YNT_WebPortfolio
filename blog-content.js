211d20a55a805010c3170c02c892eb956213ca56
ynt29tie <ynt29tie@gmail.com>
2021-09-22 23:56:25 +0700

    add the same files but different path

---
+++ blog-content.js
@@ -0,0 +1,87 @@
+let blogText = document.getElementById("blog-content") 
+let content = [
+    {
+        name : "ลูกทุ่ง",
+        subtitle : "ไอ้หนุ่มรถไถ"
+    },
+    {
+        name : "ลูกกรุง",
+        subtitle : "เรือนแพ"
+    },
+    {
+        name : "ลูกกรุง",
+        subtitle : "เรือนแพ"
+    },
+    {
+        name : "ลูกกรุง",
+        subtitle : "เรือนแพ"
+    },
+    {
+        name : "ลูกกรุง",
+        subtitle : "เรือนแพ"
+    },
+    {
+        name : "ลูกกรุง",
+        subtitle : "เรือนแพ"
+    },
+    {
+        name : "ลูกกรุง",
+        subtitle : "เรือนแพ"
+    },
+    {
+        name : "ลูกกรุง",
+        subtitle : "เรือนแพ"
+    },
+    {
+        name : "ลูกกรุง",
+        subtitle : "เรือนแพ"
+    },
+    {
+        name : "ลูกกรุง",
+        subtitle : "เรือนแพ"
+    },
+    {
+        name : "ลูกสาว(แฟน)",
+        subtitle : "Myujikki"
+    }
+    
+    
+] 
+// for(var i = 0; i < content.length; i ++){
+//     console.log(content[i])
+//     console.log(blogText)
+//     if (i%2 == 0) {
+//         blogText.innerHTML += 
+//         `
+//         ${
+//             Math.random()*100
+//         }
+//         <h5 class="my-5">
+//         ${
+//             content[i].name 
+//         }
+//         </h5>
+//         <div class="bg-danger">
+//         ${
+//             content[i].subtitle 
+//         }
+//         </div>
+//         `
+//     }
+//     else {
+//         blogText.innerHTML += 
+//         `
+//         <h5 class="my-5">
+//         ${
+//             content[i].name 
+//         }
+//         </h5>
+//         <div class="bg-success">
+//         ${
+//             content[i].subtitle 
+//         }
+//         </div>
+//         `
+//     }
+
+// }
