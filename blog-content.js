471f7db91849814a53fb0aa2e755e1b2f7f6abcb
ynt29tie <ynt29tie@gmail.com>
2021-09-23 01:32:58 +0700

    update 23/9/2021

--- blog-content.js
+++ blog-content.js
@@ -1,87 +1,62 @@
+/* ทำการดึง Data มา เก็บไว้ในตัวแปร song*/
 let blogText = document.getElementById("blog-content") 
-let content = [
-    {
-        name : "ลูกทุ่ง",
-        subtitle : "ไอ้หนุ่มรถไถ"
-    },
-    {
-        name : "ลูกกรุง",
-        subtitle : "เรือนแพ"
-    },
-    {
-        name : "ลูกกรุง",
-        subtitle : "เรือนแพ"
-    },
-    {
-        name : "ลูกกรุง",
-        subtitle : "เรือนแพ"
-    },
-    {
-        name : "ลูกกรุง",
-        subtitle : "เรือนแพ"
-    },
-    {
-        name : "ลูกกรุง",
-        subtitle : "เรือนแพ"
-    },
-    {
-        name : "ลูกกรุง",
-        subtitle : "เรือนแพ"
-    },
-    {
-        name : "ลูกกรุง",
-        subtitle : "เรือนแพ"
-    },
-    {
-        name : "ลูกกรุง",
-        subtitle : "เรือนแพ"
-    },
-    {
-        name : "ลูกกรุง",
-        subtitle : "เรือนแพ"
-    },
-    {
-        name : "ลูกสาว(แฟน)",
-        subtitle : "Myujikki"
-    }
-    
-    
-] 
-// for(var i = 0; i < content.length; i ++){
-//     console.log(content[i])
-//     console.log(blogText)
-//     if (i%2 == 0) {
-//         blogText.innerHTML += 
-//         `
-//         ${
-//             Math.random()*100
-//         }
-//         <h5 class="my-5">
-//         ${
-//             content[i].name 
-//         }
-//         </h5>
-//         <div class="bg-danger">
-//         ${
-//             content[i].subtitle 
-//         }
-//         </div>
-//         `
-//     }
-//     else {
-//         blogText.innerHTML += 
-//         `
-//         <h5 class="my-5">
-//         ${
-//             content[i].name 
-//         }
-//         </h5>
-//         <div class="bg-success">
-//         ${
-//             content[i].subtitle 
-//         }
-//         </div>
-//         `
-//     }
+let song = fetch("./data.json")
+/* then เพราะ data ตอนนี้เป็น raw data*/
+.then(function(response) {
+/* แปลงจาก raw เป็น JSON*/
+   return response.json();
+})
+.then(data =>  {return data});
+// let cokwid = fetch("https://covid19.ddc.moph.go.th/api/Cases/today-cases-all")
+// .then(function(response) {
+//     return response.json();
+// })
+// .then(data => {return data});
 
-// }
+// cokwid.then(function(response) {
+//     console.log(response)
+// })
+console.log(song)
+
+song.then(function(response) {
+    console.log(response)
+    for(var i = 0; i < response.data.length; i ++){
+    console.log(blogText)
+    if (i%2 == 0) {
+        blogText.innerHTML += 
+        `
+        ${
+            Math.random()*100
+        }
+        <h5 class="my-5">
+        ${
+            response.data[i] 
+        }
+        </h5>
+        <div class="bg-danger">
+        ${
+            response.data[i].name + " " + "เพลง " +
+            response.data[i].subtitle 
+        }
+        </div>
+        `
+    }
+    else {
+        blogText.innerHTML += 
+        `
+        <h5 class="my-5">
+        ${
+            response.data[i]
+        }
+        </h5>
+        <div class="bg-success">
+        ${
+            response.data[i].name + " " + "เพลง " +
+            response.data[i].subtitle 
+        }
+        </div>
+        `
+    }
+
+}
+})
