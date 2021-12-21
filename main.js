01793269b17b5013e533a09e6bf8697871461233
ynt29tie <ynt29tie@gmail.com>
2021-12-21 14:04:38 +0700

    update pages

---
+++ main.js
@@ -0,0 +1,25 @@
+$(document).ready (
+	$("#forward-button-1").click(function () {
+		var value = $(this).html();
+		if (value == "Next") {
+			$(".aboutme-content-1").css("display", "none");
+			$(".aboutme-content-2").css("display", "flex");
+		}
+	}),
+
+	$("#backward-button-1").click(function () {
+		var value = $(this).html();
+		if (value == "Back") {
+			$(".aboutme-content-2").css("display", "none");
+			$(".aboutme-content-1").css("display", "flex");
+		}
+	}),
+
+	$("#forward-button-2").click(function () {
+		var value = $(this).html();
+		if (value == "Next") {
+			$(".aboutme-content-2").css("display", "none");
+			$(".aboutme-content-3").css("display", "flex");
+		}
+	})
+)
